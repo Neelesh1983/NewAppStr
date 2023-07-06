2f247e1e301ae9faa18dab61c8a8d038eab524e3
Neelesh Bohre <109515400+Neelesh1983@users.noreply.github.com>
2023-07-06 21:48:07 +0530

    Update snowconnect.py

--- snowconnect.py
+++ snowconnect.py
@@ -2,7 +2,13 @@ import streamlit as st
 import pandas as pd
 import snowflake.connector as sncon
 
-con1 = sncon.connect(**st.secrets["snowflake"])
+con1 = sncon.connect(
+  user = 'Neelesh2023',
+  password = 'Neelesh@2023',
+  account = 'vi98550.ap-southeast-1.aws',
+  warehouse = 'COMPUTE_WH',
+  database = 'COVID19_EPIDEMIOLOGICAL_DATA',
+  schema = 'Public')
 cur1 = con1.cursor()
 cur1.execute("select top 100 state, county from DEMOGRAPHICS")
 
