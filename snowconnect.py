dfbbac792a2ea7ffda0e3e9898a87f5c13215b2b
Neelesh Bohre <109515400+Neelesh1983@users.noreply.github.com>
2023-07-06 21:41:35 +0530

    Update snowconnect.py

--- snowconnect.py
+++ snowconnect.py
@@ -2,8 +2,3 @@ import streamlit as st
 import pandas as pd
 
 st.title('View Raw Data')
-
-import snowflake.connector
-from snowflake.connector import ProgrammingError
-
-con = snowflake.connector.connect(**st.secrets["snowflake"])
