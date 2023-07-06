1c2546beabdd737771e8c1d37370e3edf6a4ccfb
Neelesh Bohre <109515400+Neelesh1983@users.noreply.github.com>
2023-07-06 22:39:11 +0530

    Update snowconnect.py

--- snowconnect.py
+++ snowconnect.py
@@ -4,6 +4,6 @@ import snowflake.connector
 
 con1 = snowflake.connector.connect(**st.secrets["snowflake"])
 cur1 = con1.cursor()
-cur1.execute("select top 100 state, county from DEMOGRAPHICS")
+cur1.execute("select top 100 state, county from COVID19_EPIDEMIOLOGICAL_DATA.PUBLIC.DEMOGRAPHICS")
 
 st.title('View Raw Data')
