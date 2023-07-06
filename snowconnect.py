4cc8efd016931d55c146d22ca69218938af28752
Neelesh Bohre <109515400+Neelesh1983@users.noreply.github.com>
2023-07-06 21:43:25 +0530

    Update snowconnect.py

--- snowconnect.py
+++ snowconnect.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
+import snowflake.connector as sncon
+
+con1 = sncon.connect(**st.secrets["snowflake"])
+cur1 = con1.cursor()
+cur1.execute("select top 100 state, county from DEMOGRAPHICS")
 
 st.title('View Raw Data')
